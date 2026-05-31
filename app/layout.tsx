e9b5a8992d0a1f9bc56dc5692bfbd560df2f2de9
Uwamari Goddey <positiveimpactmakers@yahoo.com>
2026-05-31 19:05:37 -0400

    feat(analytics): add Google Analytics 4 (G-Q799EVB9BT)
    Added @next/third-parties GoogleAnalytics component to app/layout.tsx.
    Tracks all page views across getdevcontrol.com automatically.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Toaster } from "sonner";
 import { Providers } from "./providers";
 import "./globals.css";
@@ -34,6 +35,7 @@ export default function RootLayout({
           {children}
         </Providers>
         <Toaster />
+        <GoogleAnalytics gaId="G-Q799EVB9BT" />
       </body>
     </html>
   );
